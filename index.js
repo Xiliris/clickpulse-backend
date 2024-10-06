060b7ac11f84094ebabdbc0d2b32c6eb6ca7e01c
Adnan Skopljak <trashyface22@gmail.com>
2024-10-06 19:31:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
-const sendVerifyEmail = require("./modules/sendVerifyEmail");
 
 const RouteHandler = require("@xiliris/express-route-handler");
 const path = require("path");
@@ -28,7 +27,6 @@ routeHandler.handleRoutes();
 
 app.listen(PORT, async () => {
   console.log(`> Started on port: ${PORT}`);
-  sendVerifyEmail("adnanskopljak420@gmail.com", "42069");
 
   try {
     await database.query("SELECT 1");

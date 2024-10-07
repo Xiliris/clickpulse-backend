2021a4182b0a7be4a5f8869d78f43a912a7113bb
Xiliris <trashyface22@gmail.com>
2024-10-07 20:25:58 +0200

    fix: bugs

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
 
 const RouteHandler = require("@xiliris/express-route-handler");
-const path = require("path");
 const routePath = path.join(__dirname, "routes");
 const routeData = {
   log: true,
 };
 
 const database = require("./database/mysql");
-
-app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
 const routeHandler = new RouteHandler(app, routePath, routeData);
 
